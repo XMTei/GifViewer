1cb6bf2b8179875b0bea410333c9eee236a8542d
Tei <tei@infbj.com>
2018-01-11 07:52:30 +0800

    upgrade to ASP.NET Core 2.0

--- GifViewer/Program.cs
+++ GifViewer/Program.cs
@@ -3,22 +3,23 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace GifViewer
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
+		public static void Main(string[] args)
+		{
+			BuildWebHost(args).Run();
+		}
 
-            host.Run();
-        }
-    }
+		public static IWebHost BuildWebHost(string[] args) =>
+			WebHost.CreateDefaultBuilder(args)
+				.UseStartup<Startup>()
+				.Build();
+	}
 }
